55b0ac38578a89cc10bf7d2270a6060617309a97
satnam1994 <singh.sat111@gmail.com>
2018-04-04 22:48:41 +0530

    Add all files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+socket.on(‘event name’, function(msg){});
+socket.emit('event name', message);
+
+var app=angular.module('myApp',[]);
+app.controller('mainController',['$scope',function($scope){
+ var socket = io.connect();
+ $scope.send = function(){
+  socket.emit('chat message', $scope.message);
+  $scope.message="";
+ }
+ socket.on('chat message', function(msg){
+  var li=document.createElement("li");
+  li.appendChild(document.createTextNode(msg));
+  document.getElementById("messages").appendChild(li);
+ });
+}]);
